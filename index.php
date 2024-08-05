8ff6fa7c7e946df239fe06f9e8f63273a7d67885
luix1703 <luisgbf17@gmail.com>
2024-08-05 23:13:16 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+echo "Nuestro Pimer Archivo php";
+echo '<br>';
+print ("La RECOLETA");
+echo '<br>';
+print (" AREQUIPA 2024"); 
+
+$num1 = "10" ;
+$num2 = "5" ;
+$num3 = "-5" ;
+echo '<br>';
+echo '<br>';
+echo "los numeros son : ";
+echo '<br>';
+echo "$num1, <br> $num2, <br> $num3 <br>"; 
+
+$suma = $num1 + $num2;
+$resta = $num1 - $num2;
+$multiplicacion = $num1 * $num2;
+$divicion = $num1 / $num2;
+$potencia = pow($num3,2);
+$absoluto = abs($num3);
+$num3 = "-5" ;
+$num3++;
+$num3--;
+echo "La Suma del numero1 y numero2 es: $suma <br> La Resta del numero1 y numero2 es: $resta <br> La Multiplicacion del numero1 y numero2 es: $multiplicacion <br> La Divicion del numero1 y numero2 es: $divicion <br>La Potencia del numero 3 es $potencia <br> El Valor Absoluto del numero 3 es: $absoluto ";
+?>
